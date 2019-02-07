6cc3dec23ff3ea23a2761bf82fc17613d1e0078d
Brian Wilson <Brian.wilson1367@gmail.com>
2019-02-07 15:16:15 -0500

    my work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
    root 'homes#index'
   devise_for :users
+  root 'orders#index'
+
+  resources :orders do
+    resources :parts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
